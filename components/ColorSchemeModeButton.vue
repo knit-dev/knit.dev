24e7b80a91b7fc28c303fc6d95079e75568bc1cd
tatommyttmy <tommy@knit.dev>
2020-05-19 14:14:17 -0400

    Add ColorSchemeModeButton

---
+++ components/ColorSchemeModeButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-tooltip top>
+      <template v-slot:activator="{ on }">
+        <v-btn icon height="48px" width="48px" v-on="on" @click="toggle()">
+          <v-icon>
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </template>
+      <span>{{ text }}</span>
+    </v-tooltip>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'ColorSchemeModeButton',
+  props: {
+    icon: {
+      type: String
+    },
+    text: {
+      type: String
+    },
+    toggle: {
+      type: Function
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
