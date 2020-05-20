736b027916c396b4f54d7714e260a11c79683ce2
tatommyttmy <tommy@knit.dev>
2020-05-20 15:23:01 -0400

    Fix dummy test

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
 import { mount } from '@vue/test-utils'
-import Logo from '@/components/Logo.vue'
+import VueToNuxtLogo from '@/components/VueToNuxtLogo.vue'
 
 describe('VueToNuxtLogo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
+    const wrapper = mount(VueToNuxtLogo)
     expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
