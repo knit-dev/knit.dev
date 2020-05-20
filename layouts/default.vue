d003ca07eb3db5b4be7ad40438b015284c2e425c
tatommyttmy <tommy@knit.dev>
2020-05-20 02:09:43 -0400

    Remove footer dummy text

--- layouts/default.vue
+++ layouts/default.vue
@@ -95,7 +95,7 @@
     </v-content>
 
     <v-footer padless>
-      <v-card flat tile class="text-center">
+      <v-card flat tile class="text-center" width="100%">
         <v-card-text>
           <ColorSchemeModeButton
             v-bind="{
@@ -107,17 +107,6 @@
           <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
             <v-icon size="24px">{{ icon }}</v-icon>
           </v-btn>
-        </v-card-text>
-
-        <v-card-text class="pt-0">
-          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.
-          Mauris cursus commodo interdum. Praesent ut risus eget metus luctus
-          accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim
-          a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula
-          lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus
-          iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum
-          tempor vel ut orci. Orci varius natoque penatibus et magnis dis
-          parturient montes, nascetur ridiculus mus.
         </v-card-text>
 
         <v-divider></v-divider>
