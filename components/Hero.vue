89688656edbdd940516252f2ab5c4b58b4c716ca
jwtong <jeremy@knit.dev>
2020-06-09 15:57:39 -0700

    style svg inline

--- components/Hero.vue
+++ components/Hero.vue
@@ -33,16 +33,129 @@
         </div>
       </v-col>
       <v-col cols="12" md="6" class="pa-0">
-        <div class="hero-illustration my-5 ml-10 pr-10">
-          <div class="hero-image-wrapper">
-            <v-img
-              contain
-              aspect-ratio="1"
-              class="hero-image"
-              :src="content.image"
-              :lazy-src="content.lazyImage"
-              alt="hero illustration"
-            ></v-img>
+        <div class="svg-wrapper ml-10">
+          <svg
+            width="730"
+            height="819"
+            viewBox="0 0 730 819"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            class="lines"
+          >
+            <g mask="url(#mask0)">
+              <path
+                d="M-454.502 -104.101L243.432 822.956"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 -53.9893L243.013 872.511"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 -3.87823L243.432 923.179"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 46.2328L243.432 973.29"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 96.344L243.432 1023.4"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 146.455L243.432 1073.51"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 196.566L243.432 1123.62"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 246.678L243.432 1173.73"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 296.789L243.432 1223.85"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M-454.502 346.9L243.432 1273.96"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M941.366 -104.101L224.569 848.012"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M941.366 -53.9893L224.569 898.123"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M941.366 -3.87823L224.569 948.234"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M941.366 46.2328L224.569 998.345"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M971.525 56.375L224.776 1048.81"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M1017.11 45.6169L224.776 1098.12"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M1051.76 50.0994L224.776 1148.33"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M1090.06 49.2029L224.776 1198.53"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M1127.44 49.2029L224.776 1248.74"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+              <path
+                d="M1167.56 46.5133L243.012 1273.84"
+                stroke="#D8BCFD"
+                stroke-miterlimit="10"
+              />
+            </g>
+          </svg>
+          <div class="hero-illustration mt-5 pr-12">
+            <div class="hero-image-wrapper">
+              <v-img
+                contain
+                aspect-ratio="1"
+                class="hero-image"
+                :src="content.image"
+                :lazy-src="content.lazyImage"
+                alt="hero illustration"
+              ></v-img>
+            </div>
           </div>
         </div>
       </v-col>
@@ -73,6 +186,7 @@ export default defineComponent({
   -webkit-filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.22));
   filter: drop-shadow(5px 5px 5px rgba(0, 0, 0, 0.22));
 }
+
 .hero-image-wrapper {
   background: linear-gradient(
     118.61deg,
@@ -81,16 +195,23 @@ export default defineComponent({
   );
   border-radius: 35px;
 }
-.hero-illustration {
-  background-size: cover;
-  background-position: left;
-  background-image: url('~assets/images/knit/textures/hero-texture.svg');
+
+.svg-wrapper {
+  position: relative;
+  overflow: hidden;
+}
+
+.lines {
+  position: absolute;
+  display: inline-block;
+  width: 100%;
+  height: 120%;
 }
 
 path {
-  stroke-dasharray: 1000;
-  stroke-dashoffset: 1000;
-  animation: dash 2s linear forwards;
+  stroke-dasharray: 2000;
+  stroke-dashoffset: 2000;
+  animation: dash 3s linear forwards;
 }
 
 @keyframes dash {
