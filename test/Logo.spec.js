c213f524ec0f97ba39c45f5e07eb3a4199091f37
tatommyttmy <tommy@knit.dev>
2020-06-14 13:53:23 -0400

    Upgrade eslint packages. Downgrade jest packages. Remove dummy test

--- test/Logo.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import VueToNuxtLogo from '@/components/VueToNuxtLogo.vue'
-
-describe('VueToNuxtLogo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(VueToNuxtLogo)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
-})
