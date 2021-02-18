6f8936c245fc8d33f1004115419b97ee60fdcea3
Tommy Clark <tommy@knit.dev>
2021-02-18 02:15:13 -0800

    Remove generate from config

--- nuxt.config.js
+++ nuxt.config.js
@@ -143,9 +143,4 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: { extractCSS: process.env.NODE_ENV === 'production' },
-
-  generate: {
-    fallback: true,
-    interval: 2000,
-  },
 }
